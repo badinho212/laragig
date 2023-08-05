45de2ef58c367d6e7b53f0d46ecc7bf772d73ad6
badinho212 <badinho212@gmail.com>
2023-08-05 22:11:40 +0300

    first commit

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,34 +1,11 @@
-<?php 
+<?php
 
 namespace App\Models;
 
-class Listing {
-	public static function all(){
-		return  [
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => 'Here is where you can register web routes for your application. These
-                    routes are loaded by the RouteServiceProvider within a group which
-                    contains the'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'description' => 'Here is where you can register web routes for your application. These
-                    routes are loaded by the RouteServiceProvider within a group which
-                    contains the'
-            ]
-        ];
-	}
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-	public static function find($id) {
-		$listings = self::all();
-
-		foreach($listings as $listing){
-			if($listing['id'] == $id){
-				return $listing;
-			}
-		}
-	}
+class Listing extends Model
+{
+    use HasFactory;
 }
