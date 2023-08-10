28632c2f0f0d3cde822e3824e243ccbce67d14c3
badinho212 <badinho212@gmail.com>
2023-08-10 09:27:03 +0300

    CRUDE section completed

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Validation\Rule;
 use Illuminate\Http\Request;
 use App\Models\Listing;
 
@@ -10,7 +10,7 @@ class ListingController extends Controller
     //show all listings
    public function index(){
      return view('listings.index',[
-        'listings' => Listing::latest()->filter(request(['tag', 'search']))->get()
+        'listings' => Listing::latest()->filter(request(['tag', 'search']))->paginate(6)
     ]);
    }
 
@@ -19,6 +19,70 @@ class ListingController extends Controller
         return view('listings.show',[
             'listing' => $listing
         ]);
+   }
+
+   //show Create form
+   public function create() {
+    return view('listings.create');
+   }
+
+
+   //store listing data
+   public function store(Request $request) {
+    $formFields = $request->validate([
+        'title' => 'required',
+        'company' => ['required', Rule::unique('listings','company')],
+        'location' => 'required',
+        'website' => 'required',
+        'email' => ['required', 'email'],
+        'tags' => 'required',
+        'description' => 'required'
+    ]);
+
+    if($request->hasFile('logo')) {
+        $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+    }
+
+    Listing::create($formFields);
+
+    return redirect('/')->with('message', 'Listing created successfull!');
     
    }
+
+      //Show Edit form
+   public function edit(Listing $listing) {
+
+    return view('listings.edit', ['listing' => $listing]);
+
+   }
+
+   //update listing data
+   public function update(Request $request, Listing $listing) {
+    $formFields = $request->validate([
+        'title' => 'required',
+        'company' => 'required',
+        'location' => 'required',
+        'website' => 'required',
+        'email' => ['required', 'email'],
+        'tags' => 'required',
+        'description' => 'required'
+    ]);
+
+    if($request->hasFile('logo')) {
+        $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+    }
+
+    $listing->update($formFields);
+
+    return back()->with('message', 'Listing updated successfull!');
+    
+   }
+
+   public function destroy(Listing $listing){
+        $listing->delete();
+
+        return redirect('/')->with('message', 'Listing deleted successfull');
+   }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,6 @@ use App\Http\Controllers\ListingController;
 |
 */
 
-// All Listings
-Route::get('/',[ListingController::class, 'index']);
-
-
-// Single Listing
-Route::get('/listings/{listing}', [ListingController::class, 'show']);
-
 /**
 Common Resource Routes:
 index - Show all listings
@@ -28,3 +21,24 @@ show - Show single listings
 create - Show form to create new listings
 update -Update listings
 destroy - Delete listing */
+
+// All Listings
+Route::get('/',[ListingController::class, 'index']);
+
+// Show create form
+Route::get('/listings/create', [ListingController::class, 'create']);
+
+// Show create form
+Route::post('/listings', [ListingController::class, 'store']);
+
+// Show Edit Form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+
+// update listings
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
+
+// delete listings
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
+
+// Single Listing
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
